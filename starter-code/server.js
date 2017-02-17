e52c722d9c1a138d85faac3708ada763d10e26d0
Sam Hamm <samhamm@gmail.com>
2017-02-17 08:13:04 -0800

    updated lab assignment for Friday morning

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,22 +1,23 @@
 'use strict';
 
+// TODO: Do not forget to go into your SQL shell and DROP TABLE the existing articles/authors tables. Be sure to start clean.
 const pg = require('pg');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';// TODO: Don't forget to set your own conString
+// TODO: Don't forget to set your own conString if required by your system
+const conString = 'postgres://localhost:5432';
+// TODO: Using a sentence or two, describe what is happening in Line 12.
+// Put your response here...
 const client = new pg.Client(conString);
-client.connect(function(err) {
-  if (err) console.error(err);
-});
+client.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
-
-// NOTE: Routes for requesting HTML resources
+// Routes for requesting HTML resources
 app.get('/', function(request, response) {
   response.sendFile('index.html', {root: '.'});
 });
@@ -25,9 +26,13 @@ app.get('/new', function(request, response) {
   response.sendFile('new.html', {root: '.'});
 });
 
-// NOTE: Routes for making API calls to enact CRUD Operations on our database
+// Following are the routes for making API calls to enact CRUD Operations on our database
+
+// TODO: Some of the following questions will refer back to the image called 'full-stack-diagram' that has been added to the lab directory. In that image you will see that the various parts of the application's activity have been numbered 1-5. When prompted in the following questions, identify which number best matches the location of a given process. For instance, the following line of code, where the server is handling a request from the view layer, would match up with #2.
 app.get('/articles', function(request, response) {
   // REVIEW: We now have two queries which create separate tables in our DB, and reference the authors in our articles.
+  // TODO: What number in the full-stack diagram best matches what is happening in lines 35-42?
+  // Put your response here...
   client.query(`
     CREATE TABLE IF NOT EXISTS
     authors (
@@ -46,10 +51,18 @@ app.get('/articles', function(request, response) {
       "publishedOn" DATE,
       body TEXT NOT NULL
     );`
-  )
+  ) // TODO: Referring to lines 45-52, answer the following questions:
+    // What is a primary key?
+    // Put your response here...
+    // +++++++++++++++++++++
+    // What does VARCHAR mean?
+    // Put your response here...
+    // +++++++++++++++++++++
   // REVIEW: This query will join the data together from our tables and send it back to the client.
-  client.query(
-    ``, // TODO: Write a SQL query which inner joins the data from articles and authors from all records
+  client.query(`
+    SELECT * FROM articles
+    INNER JOIN authors
+      ON articles.author_id=authors.author_id;`, // DONE: Write a SQL query which inner joins the data from articles and authors from all records
     function(err, result) {
       if (err) console.error(err);
       response.send(result.rows);
@@ -57,10 +70,12 @@ app.get('/articles', function(request, response) {
   );
 });
 
+// TODO: How is a 'post' route different than a 'get' route?
+// Put your answer here...
 app.post('/articles', function(request, response) {
   client.query(
-    '', // TODO: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
-    [], // TODO: Add the author and "authorUrl" as data for the SQL query
+    'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING', // DONE: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
+    [request.body.author, request.body.authorUrl], // DONE: Add the author and "authorUrl" as data for the SQL query
     function(err) {
       if (err) console.error(err)
       queryTwo() // This is our second query, to be executed when this first query is complete.
@@ -69,8 +84,10 @@ app.post('/articles', function(request, response) {
 
   function queryTwo() {
     client.query(
-      ``, // TODO: Write a SQL query to retrieve the author_id from the authors table for the new article
-      [], // TODO: Add the author name as data for the SQL query
+      // TODO: What is the purpose of the $1 in the following line of code?
+      // Put your response here...
+      `SELECT author_id FROM authors WHERE author=$1`, // DONE: Write a SQL query to retrieve the author_id from the authors table for the new article
+      [request.body.author], // DONE: Add the author name as data for the SQL query
       function(err, result) {
         if (err) console.error(err)
         queryThree(result.rows[0].author_id) // This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query
@@ -79,11 +96,21 @@ app.post('/articles', function(request, response) {
   }
 
   function queryThree(author_id) {
+      // TODO: What number in the full-stack diagram best matches what is happening in line 100?
     client.query(
-      ``, // TODO: Write a SQL query to insert the new article using the author_id from our previous query
-      [], // TODO: Add the data from our new article, including the author_id, as data for the SQL query.
+      `INSERT INTO
+      articles(author_id, title, category, "publishedOn", body)
+      VALUES ($1, $2, $3, $4, $5);`, // DONE: Write a SQL query to insert the new article using the author_id from our previous query
+      [
+        author_id,
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body
+      ], // DONE: Add the data from our new article, including the author_id, as data for the SQL query.
       function(err) {
         if (err) console.error(err);
+        // TODO: What number in the full-stack diagram best matches what is happening in line 114?
         response.send('insert complete');
       }
     );
@@ -92,8 +119,8 @@ app.post('/articles', function(request, response) {
 
 app.put('/articles/:id', function(request, response) {
   client.query(
-    ``, // TODO: Write a SQL query to retrieve the author_id from the authors table for the new article
-    [], // TODO: Add the author name as data for the SQL query
+    `SELECT author_id FROM authors WHERE author=$1`, // DONE: Write a SQL query to retrieve the author_id from the authors table for the new article
+    [request.body.author], // DONE: Add the author name as data for the SQL query
     function(err, result) {
       if (err) console.error(err)
       queryTwo(result.rows[0].author_id)
@@ -103,15 +130,27 @@ app.put('/articles/:id', function(request, response) {
 
   function queryTwo(author_id) {
     client.query(
-      ``, // TODO: Write a SQL query to update an existing author record
-      [] // TODO: Add the values for this table as data for the SQL query
+      // TODO: In a sentence or two, describe how a SQL 'UPDATE' is different from an 'INSERT', and identify which REST verbs and which CRUD components align with them.
+      `UPDATE authors
+      SET author=$1, "authorUrl"=$2
+      WHERE author_id=$3;`, // DONE: Write a SQL query to update an existing author record
+      [request.body.author, request.body.authorUrl, author_id] // DONE: Add the values for this table as data for the SQL query
     )
   }
 
   function queryThree(author_id) {
     client.query(
-      ``, // TODO: Write a SQL query to update an existing article record
-      [], // TODO: Add the values for this table as data for the SQL query
+      `UPDATE articles
+      SET author_id=$1, title=$2, category=$3, "publishedOn"=$4, body=$5
+      WHERE article_id=$6;`, // DONE: Write a SQL query to update an existing article record
+      [
+        author_id,
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+        request.params.id
+      ], // DONE: Add the values for this table as data for the SQL query
       function(err) {
         if (err) console.error(err);
         response.send('insert complete');
@@ -120,11 +159,16 @@ app.put('/articles/:id', function(request, response) {
   }
 });
 
+  // TODO: What number in the full-stack diagram best matches what is happening in line 163?
 app.delete('/articles/:id', function(request, response) {
+    // TODO: What number in the full-stack diagram best matches what is happening in lines 165?
   client.query(
     `DELETE FROM articles WHERE article_id=$1;`,
+    // TODO: What does the value in 'request.params.id' come from? If unsure, look in the Express docs.
+    // Put your response here...
     [request.params.id]
   );
+  // TODO: What number in the full-stack diagram best matches what is happening in line 171?
   response.send('Delete complete');
 });
 
@@ -138,3 +182,5 @@ app.delete('/articles', function(request, response) {
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}!`);
 });
+
+// TODO: Make your own drawing of the full-stack diagram on a blank piece of paper (there is a stack of paper on the table next to the door into our classroom) and submit to the TA who grades your lab assignments. This is for just a little extra reinforcement of how everything works.
